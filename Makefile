208e4a26ccd634cc9e21c8467830648bf03e7283
phucvt <phucvt@vng.com.vn>
2023-05-04 16:11:39 +0700

    [*] Add unit test for tokenization API

---
+++ Makefile
@@ -0,0 +1,10 @@
+build: openapi-generator-cli generate \
+    		-i spec/zlp.yaml \
+    		-g typescript-node \
+    		-t templates/typescript \
+    		-o build \
+    		--global-property models,supportingFiles \
+    		--additional-properties=serviceName=Tokenization \
+    		--additional-properties=modelPropertyNaming=original \
+    		--additional-properties=helperFunctions=templates/helpers/camelCase.ts
+    cp build/model/* typings
--- src/zalopay/__tests__/tokenization/tokenization.test.ts
+++ src/zalopay/__tests__/tokenization/tokenization.test.ts
@@ -2,8 +2,18 @@ import {ZaloPayClient} from "../../zaloPayClient";
 import {TokenizationAPI} from "../../services";
 import nock from "nock";
 import {createClient} from "../__mocks__/base";
-import {bindingSuccess} from "../__mocks__/tokenization/responses";
+import {
+    bindingSuccess,
+    queryBalanceSuccess,
+    queryBindingSuccess, queryUserSuccess, submitPayProcessing,
+    unbindSuccess
+} from "../__mocks__/tokenization/responses";
 import {AgreementBindRequest} from "../../../../typings/agreementBindRequest";
+import {AgreementUnbindRequest} from "../../../../typings/agreementUnbindRequest";
+import {AgreementQueryRequest} from "../../../../typings/agreementQueryRequest";
+import {AgreementBalanceRequest} from "../../../../typings/agreementBalanceRequest";
+import {AgreementPayRequest} from "../../../../typings/agreementPayRequest";
+import {AgreementQueryUserRequest} from "../../../../typings/agreementQueryUserRequest";
 
 let client: ZaloPayClient;
 let tokenizationAPI: TokenizationAPI;
@@ -15,7 +25,7 @@ beforeEach((): void => {
     }
     client = createClient();
     tokenizationAPI = new TokenizationAPI(client);
-    scope = nock(`https://sb-openapi.zalopay.vn/v2`);
+    scope = nock(`https://sb-openapi.zalopay.vn`);
 });
 
 afterEach(() => {
@@ -24,7 +34,7 @@ afterEach(() => {
 
 describe("Tokenization API", (): void => {
     test("should create binding", async (): Promise<void> => {
-        scope.post("/agreement/bind")
+        scope.post("/v2/agreement/bind")
             .reply(200, bindingSuccess)
         const request = new AgreementBindRequest();
         // exclude app_id, mac
@@ -39,5 +49,78 @@ describe("Tokenization API", (): void => {
         request.req_date = 1680848533635;
         const response = await tokenizationAPI.bind(request);
         expect(response.return_code).toEqual(1);
+        expect(response.sub_return_code).toEqual(1);
+    });
+
+    test("should delete binding", async (): Promise<void> => {
+        scope.post("/v2/agreement/unbind")
+            .reply(200, unbindSuccess)
+        const request = new AgreementUnbindRequest();
+        // exclude app_id, mac
+        request.identifier = "84903863801";
+        request.binding_id = "230407qQe7vGnqp0agyforLAy0D2b1x3"
+        request.req_date = 1680848533635;
+        const response = await tokenizationAPI.unbind(request);
+        expect(response.return_code).toEqual(1);
+        expect(response.sub_return_code).toEqual(1);
+    });
+
+    test("should get payment token of a binding", async (): Promise<void> => {
+        scope.post("/v2/agreement/query")
+            .reply(200, queryBindingSuccess)
+        const request = new AgreementQueryRequest();
+        // exclude app_id, mac
+        request.app_trans_id = "230407_13221300383";
+        request.req_date = 1680848533635;
+        const response = await tokenizationAPI.query(request);
+        expect(response.return_code).toEqual(1);
+        expect(response.sub_return_code).toEqual(1);
+        expect(response.data?.pay_token).toEqual("NJLHNTRLZDETZGQYZI0ZNTBKLTGWZJKTMTKXYJHLMWI5NTFI");
+    });
+
+    test("should get the balance of user", async (): Promise<void> => {
+        scope.post("/v2/agreement/balance")
+            .reply(200, queryBalanceSuccess)
+        const request = new AgreementBalanceRequest();
+        // exclude app_id, mac
+        request.identifier = "84903863801";
+        request.pay_token = "NJLHNTRLZDETZGQYZI0ZNTBKLTGWZJKTMTKXYJHLMWI5NTFI";
+        request.amount = 50000;
+        request.req_date = 1680848533635;
+        const response = await tokenizationAPI.balance(request);
+        expect(response.return_code).toEqual(1);
+        expect(response.sub_return_code).toEqual(1);
+        expect(response.data?.length).toEqual(1);
+        expect(response.data?.[0].channel).toEqual(38);
+        expect(response.data?.[0].payable).toEqual(true);
+    });
+
+    test("should submit payment after creating order", async (): Promise<void> => {
+        scope.post("/v2/agreement/pay")
+            .reply(200, submitPayProcessing)
+        const request = new AgreementPayRequest();
+        // exclude app_id, mac
+        request.identifier = "84903863801";
+        request.pay_token = "NJLHNTRLZDETZGQYZI0ZNTBKLTGWZJKTMTKXYJHLMWI5NTFI";
+        request.zp_trans_token = "ACWH4QMH_wQUF_PxXGs4X4pg";
+        request.req_date = 1680848533635;
+        const response = await tokenizationAPI.pay(request);
+        expect(response.return_code).toEqual(3);
+        expect(response.sub_return_code).toEqual(3);
+        // expect(response.zp_trans_id).toEqual("230407_13583500399");
+        // expect(response.zp_trans_id).toEqual("230407000006575");
+    });
+
+    test("should get the the basic user information", async (): Promise<void> => {
+        scope.post("/v2/agreement/query_user")
+            .reply(200, queryUserSuccess)
+        const request = new AgreementQueryUserRequest();
+        // exclude app_id, mac
+        request.access_token = "ZJVKY2M3OTYTNZUZYY0ZZWYZLWJMNJITZJRHZMUXOWM5YMUX";
+        request.req_date = 1680848533635;
+        const response = await tokenizationAPI.queryUser(request);
+        expect(response.return_code).toEqual(1);
+        expect(response.sub_return_code).toEqual(1);
+        expect(response.phone).toEqual("****2606");
     });
 });
