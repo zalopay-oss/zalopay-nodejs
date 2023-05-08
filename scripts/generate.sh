98ff4d5c0112a67631218a6a38cdc5098bd91acf
phucvt <phucvt@vng.com.vn>
2023-05-08 14:56:48 +0700

    [*] Update generate scrips: add pull specification

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,7 +1,11 @@
+# Get latest OpenAPI specification
+git clone https://gitlab.zalopay.vn/accounting/dojo/docusaurus-playground.git
+
+# Generate models from specification
 openapi-generator-cli generate \
-		-i ./spec/zlp.yaml \
+		-i docusaurus-playground/specs/en/* \
 		-g typescript-node \
-		-t ./templates/typescript \
+		-t templates/typescript \
 		-o build \
 		--global-property models,supportingFiles \
 		--additional-properties=serviceName=Tokenization \
@@ -9,4 +13,7 @@ openapi-generator-cli generate \
 		--additional-properties=helperFunctions=./templates/helpers/camelCase.ts
 
 cp build/model/* src/zalopay/models
+
+# Clear
+rm -r docusaurus-playground
 rm -r build
